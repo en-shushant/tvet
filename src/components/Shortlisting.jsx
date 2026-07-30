ac556ffcac6a36043bac001e32299fc11d853d8d
Shushant <shushant@envisionanalytics.net>
2026-07-30 10:52:53 +0545

    fix: display:block on lh-img removes inline baseline gaps around letterhead

--- src/components/Shortlisting.jsx
+++ src/components/Shortlisting.jsx
@@ -294,7 +294,7 @@ async function openShortlistLetter(row, opts = {}) {
     padding: 0;
     overflow: hidden;
   }
-  .lh-img { position:absolute;top:0;left:0;width:100%;height:100%;object-fit:fill;z-index:0; }
+  .lh-img { display:block;position:absolute;top:0;left:0;width:100%;height:100%;object-fit:fill;z-index:0; }
   .page-inner {
     position: relative;
     z-index: 1;
